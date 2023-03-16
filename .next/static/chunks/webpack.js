0944fac6ccd2e0d2e1e7467f4658010a877cccf5
SANTHOSH-MAMIDISETTI <mamidisettisanthosh2004@gmail.com>
2023-03-16 21:47:02 +0530

    push

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f4ad0d9cb679bb94"; }
+/******/ 		__webpack_require__.h = function() { return "0385065e5ef986b6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
